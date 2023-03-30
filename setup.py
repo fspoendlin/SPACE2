466821625c47869b3fc2074a0cce5457fe1109ca
Fabian Spoendlin <f.spoendlin@bluewin.ch>
2023-03-30 15:52:02 +0100

    updated author information

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ setup(
     version='0.0.1',
     description='Set of functions to cluster CDR structures',
     license='BSD 3-clause license',
-    maintainer='Fabian Spoendlin',
+    maintainer=['Brennan Abanades','Fabian Spoendlin'],
     long_description=long_description,
     long_description_content_type='text/markdown',
-    maintainer_email='fabian.spoendlin@stats.ox.ac.uk',
+    maintainer_email=['brennan.abanadeskenyon@stx.ox.ac.uk','fabian.spoendlin@stats.ox.ac.uk'],
     include_package_data=True,
     packages=find_packages(include=('SPACE2', 'SPACE2.*')),
     install_requires=[
